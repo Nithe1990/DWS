0dcdf190dddcd04556f51365c3e6534227857c30
CristianEnderman <Clasecristian@hotmail.com>
2022-10-17 11:26:58 +0000

    Ejercicio tabla empezado

---
+++ Tema-3/tabla/php/datos.php
@@ -0,0 +1,73 @@
+<?
+    $liga =
+    array(
+        "Zamora" =>  array(
+            "Salamanca" => array(
+                "Resultado" => "3-2", "Roja" => 1, "Amarilla" => 0, "Penalti" => 0
+            ),
+            "Avila" => array(
+                "Resultado" => "4-1", "Roja" => 0, "Amarilla" => 0, "Penalti" => 0
+            ),
+            "Valladolid" => array(
+                "Resultado" => "1-2", "Roja" => 1, "Amarilla" => 1, "Penalti" => 1
+            )
+        ),
+        "Salamanca" =>  array(
+            "Zamora" => array(
+                "Resultado" => "3-2", "Roja" => 1, "Amarilla" => 0, "Penalti" => 0
+            ),
+            "Avila" => array(
+                "Resultado" => "4-1", "Roja" => 0, "Amarilla" => 0, "Penalti" => 0
+            ),
+            "Valladolid" => array(
+                "Resultado" => "1-2", "Roja" => 1, "Amarilla" => 2, "Penalti" => 1
+            )
+        ),
+        "Avila" =>  array(
+            "Zamora" => array(
+                "Resultado" => "3-2", "Roja" => 1, "Amarilla" => 0, "Penalti" => 2
+            ),
+            "Salamanca" => array(
+                "Resultado" => "1-3", "Roja" => 1, "Amarilla" => 3, "Penalti" => 0
+            ),
+            "Valladolid" => array(
+                "Resultado" => "1-3", "Roja" => 1, "Amarilla" => 0, "Penalti" => 1
+            )
+        ),
+        "Valladolid" =>  array(
+            "Zamora" => array(
+                "Resultado" => "3-2", "Roja" => 1, "Amarilla" => 0, "Penalti" => 0
+            ),
+            "Salamanca" => array(
+                "Resultado" => "3-1", "Roja" => 0, "Amarilla" => 0, "Penalti" => 0
+            ),
+            "Avila" => array(
+                "Resultado" => "1-2", "Roja" => 1, "Amarilla" => 1, "Penalti" => 2
+            )
+        ),
+    );
+    echo "<table border>";
+        // for($i = 0;$i < count($liga) +1;$i++){
+        //     echo "<tr>";
+        //     for($j = 0;$j < count($liga)+1;$j++){
+        //         echo "<td>";
+                    
+        //         echo "</td>";
+        //     }
+        //     echo "</tr>";
+        // }
+
+        echo "<tr>";
+            echo "<th>Equipos</th>";
+            foreach ($liga as $valor => $indice) {
+                echo "<th>".$valor."</th>";
+            }
+        echo "</tr>";
+        foreach ($liga as $valor => $indice) {
+            echo "<tr>";
+                echo "<td>".$valor."</td>";
+            echo "</tr>";
+        }
+    echo "</table>"
+    
+?>
