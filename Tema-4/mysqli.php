dccfaf895251e53f120a8c49cdec70c017bf0d1c
CristianEnderman <Clasecristian@hotmail.com>
2022-12-01 13:20:22 +0000

    consultas

--- Tema-4/mysqli.php
+++ Tema-4/mysqli.php
@@ -1,8 +1,23 @@
 <?
     require './conexion.php';
+    //con funciones
     try{
         $conexion = mysqli_connect(HOST,USER,PASS,"mundial");
         //$_SERVER['SERVER_ADDR']
+        $sql = "select * from equipo";
+        $sqlp = "select * from equipo where id = ? and nombre = ?";
+        $preparada = mysqli_stmt_init($conexion);
+        mysqli_stmt_prepare($preparada,$sql);
+        $resultado = mysqli_query($conexion, $sqlp,);
+        $id = 1;
+        $nombre = "España";
+        mysqli_stmt_bind_param($preparada,'i',$id);
+        mysqli_stmt_bind_param($preparada,'s',$nombre);
+        mysqli_stmt_execute($preparada);
+        //print_r($resultado->fetch_all());
+        echo "<br>";
+        // while($row = $resultado->fetch_array()) print_r($row);
+        // while($row = $resultado->fetch_object()) print_r($row);
         mysqli_close($conexion);
     }catch(Exception $ex){
         //dará 2002 en caso de que el host sea incorrecto
@@ -14,5 +29,14 @@
         if($ex->getCode() == 1045){
             echo "<br/>El usuario o la contraseña son incorrectos.";
         }
-    }   
+    }
+
+    // try {
+    //     $conexionO = new mysqli();
+    //     $conexionO -> connect($_SERVER['SERVER_ADDR'], USER, PASS, 'mundial');
+    //     $conexionO -> close();
+    // } catch (Exception $ex) {
+    //     echo $conexionO -> connect_errno;
+    //     echo $conexionO -> connect_error;
+    // }
 ?>
