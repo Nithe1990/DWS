3d4e203e7bc1dca0be1312febe2cf7d16fb9e3b9
CristianEnderman <Clasecristian@hotmail.com>
2023-03-28 08:31:20 +0000

    funciones básicas

--- Tema-5/cookies/funcionesBd.php
+++ Tema-5/cookies/funcionesBd.php
@@ -4,10 +4,14 @@
     function findAll(){
         try {
             $con = new PDO('mysql:host='.HOST.';dbname='.BBDD,USER,PASS);
-            $sql = "select * from producto";
-            $res = $con->query($sql);
-            $array = $res->fetchAll(PDO::FETCH_ASSOC);
-            unset($con);
+            $sql = "select * from producto where codigo = ?";
+            $prepare = $con -> prepare($sql);
+            $res = $prepare->execute(array($id));
+            //$array = $res->fetchAll(PDO::FETCH_ASSOC);
+            if($res){
+                unset($con);
+                $res->fetchAll();
+            }
             return $array;
         } catch (Exception $ex) {
             print_r($ex);
